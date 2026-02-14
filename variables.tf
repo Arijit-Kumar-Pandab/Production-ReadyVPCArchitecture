8d70608840c7cbbb797f598a8ad82be5ad27b1eb
Your Name <your-email@example.com>
2026-02-14 15:23:31 +0530

    Initial commit: Production-ready VPC architecture with public and private subnets

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "AWS Region"
+  type = string
+  default = "us-east-1"
+}
+
+variable "aws_version" {
+  description = "AWS Provider Version"
+  type = string
+}
