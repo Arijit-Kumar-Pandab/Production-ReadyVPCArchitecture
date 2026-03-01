ef1a4f495795e9b87b8f6e8b4e471883ad7f60e0
Your Name <your-email@example.com>
2026-03-01 18:44:57 +0530

    add default values in variable.tf

--- variables.tf
+++ variables.tf
@@ -7,14 +7,17 @@ variable "region" {
 variable "aws_version" {
   description = "AWS Provider Version"
   type        = string
+  default = "6.32.1"
 }
 
 variable "vpc_cidr_blocl" {
   description = "VPC CIDR Block"
+  default = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
   description = "VPC Name"
+  default = "production-ready-vpc"
 }
 
 variable "subnets" {
@@ -24,4 +27,30 @@ variable "subnets" {
     az   = string
     type = string
   }))
+
+  default = {
+    publicSubnet1 = {
+      cidr = "10.0.1.0/24"
+      az   = "us-east-2a"
+      type = "public"
+    }
+
+    publicSubnet2 = {
+      cidr = "10.0.2.0/24"
+      az   = "us-east-2b"
+      type = "public"
+    }
+
+    privateSubnet1 = {
+      cidr = "10.0.4.0/24"
+      az   = "us-east-2c"
+      type = "private"
+    }
+
+    privateSubnet2 = {
+      cidr = "10.0.5.0/24"
+      az   = "us-east-2a"
+      type = "private"
+    }
+  }
 }
